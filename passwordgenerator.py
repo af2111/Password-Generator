ffca50dbd51b406a91f8a3ab744b17fab4db4b67
af2111 <af@martinfritz.net>
2021-01-13 10:54:55 +0100

    added random casing

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -5,19 +5,18 @@ print('PASSWORD GENERATOR')
 while True:
     input('Hit ENTER to run.')
 
-        
-
     chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                 'u',
-                 'v', 'x', 'y', 'z', '.', ':', '#', '/', ';', '$', '|', ',']
+             'u',
+             'v', 'x', 'y', 'z', '.', ':', '#', '/', ';', '$', '|', ',']
     pwstring = ""
     pw = []
     for i in range(20):
-        pw.append(random.choice(chars))
-      
+        capitalize = random.random()
+        char = random.choice(chars)
+        if capitalize > 0.5:
+            char = char.upper()
 
-        
-
+        pw.append(char)
 
     sleep(0.5)
     print('.')
@@ -27,21 +26,3 @@ while True:
     print('...')
     sleep(0.5)
     print(pwstring.join(pw))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
